7ecc5d393ced96ad1a7bd6b422987c74315875ca
Giannis <96373640+gyfantidis@users.noreply.github.com>
2023-01-06 18:50:36 +0200

    First Commit

---
+++ DemoMaven/src/main/java/net/yfantidis/App.java
@@ -0,0 +1,13 @@
+package net.yfantidis;
+
+/**
+ * Hello world!
+ *
+ */
+public class  App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
